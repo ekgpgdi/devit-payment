f5f7f63f554550d5fc22d5fed1c528d37361679b
dahye <>
2022-06-27 10:37:11 +0900

    🎉 initial commit

---
+++ src/test/java/com/devit/devitpayment/DevitPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devit.devitpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevitPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
